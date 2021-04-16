05c708c9035b4e4fb44656a6a6e5846bf1b0ce50
Dawid Spisak <dawidspisak@gmail.com>
2021-04-16 18:39:51 +0200

    change modal styles

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import { Button, List, ListItem, ListItemAvatar, ListItemText, makeStyles, Modal } from "@material-ui/core";
+import { Button, FormControl, Input, InputLabel, List, ListItem, ListItemText, makeStyles, Modal } from "@material-ui/core";
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import './Todo.css';
 import db from './firebase';
@@ -6,12 +6,16 @@ import { useState, forwardRef } from "react";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        position: 'absolute',
         width: 400,
+        position: `absolute`,
+        top: `50%`,
+        left: `50%`,
+        transform: `translate(-50%, -50%)`,
         backgroundColor: theme.palette.background.paper,
         border: '2px solid #000',
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
+        display: 'flex',
     }
 }))
 
@@ -22,38 +26,42 @@ const Todo = forwardRef((props, ref) => {
     const classes = useStyles();
     const [input, setInput] = useState();
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
+
     const updateTodo = () => {
 
         db.collection('todos').doc(props.todo.id).set({
             todo: input
         }, { merge: true })
-
+        setInput('')
         setOpen(false);
     }
 
     return (
-        <div className="todo" ref={ref}>
-            <Modal
-                open={open}
-                onClose={e => setOpen(false)}
-            >
-                <div className={classes.paper}>
-                    <h1>I am modal</h1>
-                    <input placeholder={props.todo.todo} value={input} onChange={e => setInput(e.target.value)} />
-                    <Button onClick={updateTodo}>Update Todo</Button>
-                </div>
-            </Modal>
-            <List className="todo__list">
-                <ListItem className="todo__content-list">
-                    <ListItemText primary={props.todo.todo} secondary='Dummy deadline' className="todo__content" />
-                </ListItem>
-                <Button onClick={e => setOpen(true)} className="todo__edit-btn">Edit</Button>
-                <DeleteForeverIcon onClick={e => db.collection('todos').doc(props.todo.id).delete()} className="todo__delete-btn" />
-            </List>
+        <div>
+            <div className="todo" ref={ref}>
+                <Modal
+                    open={open}
+                    onClose={e => setOpen(false)}
+                >
+                    <div className={classes.paper}>
+                        <FormControl className="app__form-control" >
+                            <InputLabel className="app_label">Write a Todo</InputLabel>
+                            <Input placeholder={props.todo.todo} value={input} onChange={e => setInput(e.target.value)} className="todo__input" />
+                        </FormControl>
+                        <Button variant="contained" color="primary" type='submit' onClick={updateTodo} disabled={!input} className="todo__btn">
+                            Update Todo
+                </Button>
+                    </div>
+                </Modal>
+                <List className="todo__list">
+                    <ListItem className="todo__content-list">
+                        <ListItemText primary={props.todo.todo} secondary='Dummy deadline' className="todo__content" />
+                    </ListItem>
+                    <Button onClick={e => setOpen(true)} className="todo__edit-btn">Edit</Button>
+                    <DeleteForeverIcon onClick={e => db.collection('todos').doc(props.todo.id).delete()} className="todo__delete-btn" />
+                </List>
 
+            </div>
         </div>
     );
 })
